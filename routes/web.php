8f9fe32769803800c450736b7a6dd2a54e568bba
lucianoAguilarWD <lucianoaguilarwebdev@gmail.com>
2024-09-29 18:37:44 -0300

    un poco de estilos y estructuras visuales a modo de ejemplo

--- routes/web.php
+++ routes/web.php
@@ -2,44 +2,51 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Middleware\AdministradorMiddleware;
-use App\Http\Middleware\RoleMiddleware;
 use App\Http\Middleware\TechnicalMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\Models\User;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
+Route::get("/", function () {
+    return Inertia::render("Welcome", [
+        "canLogin" => Route::has("login"),
+        "canRegister" => Route::has("register"),
     ]);
 });
 
+//* entenderse como prueba previo al desarrollo de la lógica
+//? prueba de roles
+
 Route::middleware(AdministradorMiddleware::class)->group(function () {
-    //Route::get('/admin/dashboard', [AdminController::class, 'index']);
-    Route::get('/admin', function(){
-        return Inertia::render('Admin/AdminDashboard');
-    });
+    Route::get("/admin", function () {
+        return Inertia::render("Admin/AdminDashboard");
+    })->name("admin.dashboard");
 });
 
 Route::middleware(TechnicalMiddleware::class)->group(function () {
-    //Route::get('/user/dashboard', [UserController::class, 'index']);
-    Route::get('/user', function(){
-        return Inertia::render('Technical/TechnicalDashboard');
-    });
+    Route::get("/user", function () {
+        return Inertia::render("Technical/TechnicalDashboard");
+    })->name("technical.dashboard");
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+//* termina la sección de prueba
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get("/dashboard", function () {
+    return Inertia::render("Dashboard");
+})
+    ->middleware(["auth", "verified"])
+    ->name("dashboard");
+
+Route::middleware("auth")->group(function () {
+    Route::get("/profile", [ProfileController::class, "edit"])->name(
+        "profile.edit"
+    );
+    Route::patch("/profile", [ProfileController::class, "update"])->name(
+        "profile.update"
+    );
+    Route::delete("/profile", [ProfileController::class, "destroy"])->name(
+        "profile.destroy"
+    );
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . "/auth.php";
